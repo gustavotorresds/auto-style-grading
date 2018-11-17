f0550281200b76bad6b800c02e5a246dd6233500
Gustavo Torres <gustavotorresds@gmail.com>
2018-11-16 19:31:12 -0800

    Initial git commit. Start of the project. For now, only implemented util to process grades

---
+++ main.py
@@ -0,0 +1,16 @@
+import util
+
+def main():
+	grades = util.process_grades('./grades.csv')
+
+	# Do something with grades, e.g., save as .csv
+	# np.savetxt('./processed-grades.csv', grades)
+
+def svg():
+	pass
+
+def naive_bayes():
+	pass
+
+if __name__ == "__main__":
+	main()
---
+++ util.py
@@ -0,0 +1,81 @@
+import csv
+import numpy as np
+from collections import defaultdict
+
+'''
+Notes and observations
+(1) The initial grades.csv I was working with came with extra inconvenient commas ('')
+in the middle of the header. I had to erase them in order for the script to not get
+messed up when splitting data by commas. My final header looks like this:
+id,Decomposition,Commenting,Instance Variables and Parameters and Constants,Naming and Spacing,Logic and Redundancy
+'''
+
+# The impact each label has.
+# TODO: what scale should we consider?
+# TODO: when label is not found (i.e., "Random Label"), the default impact
+# ends up being zero. Is that what we want?
+LABEL_TO_GRADE = defaultdict(int)
+LABEL_TO_GRADE['Perfect'] = 7
+LABEL_TO_GRADE['Minor Issues'] = 5
+LABEL_TO_GRADE['Major Issues'] = 3
+LABEL_TO_GRADE['Horrible'] = 1
+LABEL_TO_GRADE['No comments or lots missing'] = 0
+
+BUCKETS = ['Decomposition', 'Commenting', 'Instance Variables and Parameters and Constants',\
+ 'Naming and Spacing', 'Logic and Redundancy']
+
+# The weigth each bucket should have on the grading.
+# TODO: should we weight anything heavier? How should we attribute weights?
+BUCKET_WEIGHT = defaultdict(int)
+BUCKET_WEIGHT[BUCKETS[0]] = 1
+BUCKET_WEIGHT[BUCKETS[1]] = 1
+BUCKET_WEIGHT[BUCKETS[2]] = 1
+BUCKET_WEIGHT[BUCKETS[3]] = 1
+BUCKET_WEIGHT[BUCKETS[4]] = 1
+
+def process_grades(csv_path):
+	assignment_ids, labeled_buckets = load_data(csv_path)
+
+	grades = np.array([])
+
+	for i, assignment_id in enumerate(assignment_ids):
+		grade = generate_grade(labeled_buckets[i])
+		grades = np.append(grades, grade)
+
+	return grades
+
+'''
+Uses BUCKET_WEIGHT and LABEL_TO_GRADE constants to give the grade associated
+with bucket labels for an assignment.
+
+Input:
+- np array with bucket labels.
+- E.g.: ['Minor Issues', 'Perfect', 'Minor Issues', 'Minor Issues', 'Perfect']
+
+Output:
+- Integer representing grade corresponding to the bucket labels.
+'''
+def generate_grade(labels):
+	grade = 0
+	for b, bucket in enumerate(BUCKETS):
+		label = labels[b]
+		bucket_grade = LABEL_TO_GRADE[label]
+		grade += (BUCKET_WEIGHT[bucket] * bucket_grade)
+	return grade
+
+'''
+Just opens the CSV file and returns two np arrays: one for the assignment IDs and
+another containing the bucket labels for each of the assignments (hence, a 2D array).
+'''
+def load_data(csv_path):
+	with open(csv_path, 'r') as csv_fh:
+		headers = csv_fh.readline().strip().split(',')
+
+	# Load features and labels
+	assignment_id_col = [0]
+	bucket_cols = [i for i in range(1, len(headers))]
+
+	assignment_ids = np.loadtxt(csv_path, dtype=np.dtype(str), delimiter=',', skiprows=1, usecols=assignment_id_col)
+	buckets = np.loadtxt(csv_path, dtype=np.dtype(str), delimiter=',', skiprows=1, usecols=bucket_cols)
+
+	return assignment_ids, buckets
