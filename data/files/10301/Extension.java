d8c9e8d4d7dca1cfd8bd33a9f0c8aca3688c7623
Gustavo Torres <gustavotorresds@gmail.com>
2018-11-17 18:59:07 -0800

    Update git ignore

--- data/files/10301/Extension.java
+++
@@ -1,4 +0,0 @@
-/*this is a blank file but the file listed blank file
- *  is the actually extension sorry for the inconvenience
- * its 4:30 am and I am to tired to figure out how to delete
- * this file so enjoy.
