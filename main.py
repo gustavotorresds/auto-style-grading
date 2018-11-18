b7393f3c6ae8aa5149abf9bb0ffc3b3b4599231c
Gustavo Torres <gustavotorresds@gmail.com>
2018-11-17 19:57:32 -0800

    Refactor

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
 import numpy as np
 import util
+import os
 
 def main():
-	labels = util.generate_labels('./data/grades.csv')
+	assignment_ids, _ = util.load_data('./data/grades.csv')
 
-	# Do something with grades, e.g., save as .csv
-	np.savetxt('./output/labels.txt', labels)
+	bucket = 'Decomposition'
+	labels = util.generate_labels_for_bucket('./data/grades.csv', bucket)
+
+	X = np.array([extract_features(assignment_id, bucket) for assignment_id in assignment_ids])
+	print(X)
 
 def svm():
 	pass
 
 def naive_bayes():
 	pass
+
+def extract_features(assignment_id, bucket):
+	file_path = '/'.join(['./data/files', assignment_id, 'Breakout.java'])
+	file = util.open_file(file_path)
+	
+	if bucket == 'Decomposition':
+		return decomposition_features(file)
+
+	return []
+
+'''
+[Work in Progress]
+Input: a compilable Java progrma
+Output: an array with containing
+- # of lines in file
+- # of methods in file
+- # Average of # lines per method
+'''
+def decomposition_features(file):
+	def is_a_method(line):
+		return False
+
+	line_count = 0
+	method_count = 0
+
+	# Tracks the number of lines in 
+	method_line_count_helper = 0
+	bracket_counter = 0
+
+	for line in file:
+		line_count += 1
+
+		# Are we in method-line-count mode?
+			# If so, is this the end of the method?
+				# If so, set method count and count # lines per method, and reset variables
+				# If not, just count +1
+			# Otherwise, is this line the start of a method?
+				# If it is, get into method-line-count mode
+
+	return [line_count, 0., 0.]
+
 
 if __name__ == "__main__":
 	main()
