73889c75e43ee51fcf7b43e9b543a9c55da85cc5
Gustavo Torres <gustavotorresds@gmail.com>
2018-12-13 18:28:53 -0800

    Run unsupervised over all buckets

--- unsupervised.py
+++ unsupervised.py
@@ -12,29 +12,33 @@ from sklearn.manifold import TSNE
 K = 3
 
 def main():
-	pmd_reports = load_pkl_file('data.pkl')
+	pmd_reports = load_pkl_file('data_pmd.pkl')
+	cpd_reports = load_pkl_file('data.pkl')
 
-	bucket = 'Decomposition'
-	assignment_ids, y = util.get_data(bucket, pmd_reports)
+	for bucket in ['Logic and Redundancy']:
+		print('K-Means on {}'.format(bucket))
 
-	X = np.array([extract_features(assignment_id, bucket, pmd_reports[assignment_id]) for assignment_id in assignment_ids])
-	X_embedded = TSNE(n_components=2).fit_transform(X)
+		reports = cpd_reports if bucket == 'Decomposition' else pmd_reports
+		assignment_ids, y = util.get_data(bucket, reports)
+		X = np.array([extract_features(assignment_id, bucket, reports[assignment_id]) for assignment_id in assignment_ids])
 
-	kmeans = KMeans(n_clusters=K, random_state=0).fit_predict(X_embedded)
+		X_embedded = TSNE(n_components=2).fit_transform(X)
 
-	for cluster in range(K):
-		buckets = {1: 0, 2: 0, 3: 0}
-		for i, label in enumerate(y):
-			if kmeans[i] == cluster:
-				buckets[label] += 1
-		s = buckets[1] + buckets[2] + buckets[3]
-		print('CONCENTRATIONS in cluster #{}'.format(cluster))
-		print('Major Issues: {}'.format(buckets[1] / s))
-		print('Minor Issues: {}'.format(buckets[2] / s))
-		print('Perfect: {}'.format(buckets[3] / s))
+		kmeans = KMeans(n_clusters=K, random_state=0).fit_predict(X_embedded)
 
-	plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=kmeans, cmap='viridis', alpha=0.9);
-	plt.show()
+		for cluster in range(K):
+			buckets = {1: 0, 2: 0, 3: 0}
+			for i, label in enumerate(y):
+				if kmeans[i] == cluster:
+					buckets[label] += 1
+			s = buckets[1] + buckets[2] + buckets[3]
+			print('CONCENTRATIONS in cluster #{}'.format(cluster))
+			print('Major Issues: {}'.format(buckets[1] / s))
+			print('Minor Issues: {}'.format(buckets[2] / s))
+			print('Perfect: {}'.format(buckets[3] / s))
+
+		# plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=kmeans, cmap='viridis', alpha=0.9);
+		# plt.show()
 
 
 def load_pkl_file(filename):
@@ -50,10 +54,16 @@ def extract_features(assignment_id, bucket, report=None):
 	
 	if bucket == 'Decomposition':
 		return decomposition_features(file_lines, report)
-	# TODO: implement feature extraction for other buckets.
+	elif bucket == 'Commenting':
+		return commenting_features(file_lines, report)
 	elif bucket == 'Naming and Spacing':
 		return naming_and_spacing_features(file_lines, report)
+	elif bucket == 'Instance Variables and Parameters and Constants':
+		return variable_features(file_lines, report)
+	elif bucket == 'Logic and Redundancy':
+		return logic_redundancy_features(file_lines, report)
 	else:
+		# TODO: implement feature extraction for other buckets.
 		print('Can\'t read that bucket yet :/')
 
 	return []
@@ -155,6 +165,150 @@ def decomposition_features(file, report):
 	run_length = get_run_method_length(file)
 
 	return [line_count, len(method_counts), np.mean(method_counts), np.max(method_counts), np.min(method_counts), num_repetitions, run_length]
+
+'''
+Input: a compilable Java progrma
+Output: an array containing
+- # of variables with lowerCamelCase
+- # of lines with wrong indentation
+'''
+def naming_and_spacing_features(file, report):
+	'''Removes characters that might have been attached to variable name'''
+	def variable_filter(var):
+		var = var.replace(';', '')
+		var = var.replace('=', '')
+		var = var.replace(',', '')
+
+		if '(' in var:
+			argContent = var[var.index('('):]
+			var = var.replace(argContent, '')
+
+		return var
+
+	def get_variable(line):
+		if ('static' not in line and 'final' not in line):
+			tokens = line.split()
+
+			returnNext = False
+
+			for token in tokens:
+				if returnNext:
+					return variable_filter(token)
+				if token in util.VAR_TYPES:
+					returnNext = True
+		return None
+
+	def is_camel_case(var):
+		if not var[0].islower():
+			return False
+		if '_' in var:
+			return False
+		return True
+
+	def has_right_indentation(line, indentation_level):
+		if line.strip() == '':
+			return True
+
+		i = 0
+		while line[i] == '\t':
+			i += 1
+		return i == indentation_level
+
+	def get_pmd_warns(report):
+		lines = report.split('\\n')
+		return len(lines)
+
+	indentation_level = 0
+
+	wrong_camel_case_count = 0
+	wrong_indentation_count = 0
+
+	for line in file:
+		var = get_variable(line)
+		if var and not is_camel_case(var):
+			wrong_camel_case_count += 1
+
+		if '}' in line:
+			indentation_level -= 1
+
+		if not has_right_indentation(line, indentation_level):
+			wrong_indentation_count += 1
+
+		if '{' in line:
+			indentation_level += 1
+
+	num_pmd_warns = get_pmd_warns(report)
+
+	return [wrong_camel_case_count, wrong_indentation_count, num_pmd_warns]
+
+def variable_features(file, report):
+	def extract_PMD_features(report):
+		warning_stubs = ['Avoid variables', 'Local variable', 'Parameter', 'Variables should start with', 
+			'Only variables that', 'Fields should be']
+		return [report.count(warning_stub) for warning_stub in warning_stubs]
+	def extract_number_of_variables(file):
+		num_variables = 0
+		for line in file:
+			for variable in util.VAR_TYPES:
+				if (variable in line):
+					num_variables += 1
+		return num_variables
+
+	return [extract_number_of_variables(file)] + extract_PMD_features(report)
+
+def logic_redundancy_features(file, report):
+	def extract_PMD_features(report):
+		warning_stubs = ['Avoid if ', 'A method should', 'All classes and interfaces', 
+			'Each class', 'Avoid using if', 'Use explicit scoping']
+		return [report.count(warning_stub) for warning_stub in warning_stubs]
+	return extract_PMD_features(report)
+
+def commenting_features(file, report):
+	num_comments = 0
+	ave_block_size = 0 
+	ave_comment_length = 0
+
+	def is_comment(line):
+		return '//' in line
+
+	def is_block_comment_start(line):
+		return '/**' in line or '/*' in line
+
+	def is_block_comment_end(line):
+		return '*/' in line
+	
+	code_block_list = []
+	curr_code_block_size = 0
+	num_code_blocks = 0 
+	in_comment_block_flag = 0
+	num_words = 0
+	for line in file:
+		if is_block_comment_start(line):
+			in_comment_block_flag = 1
+			num_code_blocks += 1
+			curr_code_block_size = 0
+		elif is_block_comment_end(line):
+			in_comment_block_flag = 0
+			code_block_list.append(curr_code_block_size)
+		elif in_comment_block_flag:
+			num_words += len(line.split(' '))
+			num_comments += 1
+			curr_code_block_size += 1
+		elif is_comment(line):
+			num_words += len(line.split(' '))
+			num_comments += 1
+		
+	if len(code_block_list) != 0:
+		ave_block_size = int(sum(code_block_list) / len(code_block_list))
+	else:
+		ave_block_size = 0
+	if num_comments != 0:
+		ave_comment_length = int(num_words / num_comments)
+	else:
+		ave_comment_length = 0
+
+	featureList = [num_comments, ave_block_size, ave_comment_length]
+	return featureList
 
 if __name__ == "__main__":
 	main()
